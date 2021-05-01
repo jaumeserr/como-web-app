574414e92fccf178ef0c905dc82b11f55750a1a2
Jaume Serradell <jaumeserr@gmail.com>
2021-05-01 10:21:05 +0200

    fix style CardHover

--- src/components/CardHover.js
+++ src/components/CardHover.js
@@ -2,9 +2,14 @@ import styled from 'styled-components';
 import { useState } from 'react';
 
 import SearchIcon from '../assets/search.svg';
-import { Link, Route, Switch } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import FavIco from '../components/FavIco';
 import SearchIco from '../components/SearchIco';
+
+
+import { BsHeartFill, BsSearch } from 'react-icons/bs';
+
+
 import CardDetail from '../components/CardDetail';
 import CardList from './CardList';
 
@@ -27,50 +32,29 @@ const CardHoverStyled = styled.ul`
   }
 
   li {
-    padding: 12px;
+    padding: 9px;
     margin-bottom: 10px;
-    border: 1px solid var(--primary);
     box-shadow: 1px 1px 6px 1px rgba(0,0,0,0.1);
     background-color: #ffffff;
     border-radius: 4px;
     overflow: hidden;
     cursor: pointer;
-
-    :last-child {
-      border-top: 1px solid var(--principal);
-    }
   }
 `
 
 
 const CardHover = ({ id, image, name, price, shortDescription, description, units }) => {
-  const [hoveredFav, setHoveredFav] = useState(false);
-  const [hoveredSearch, setHoveredSearch] = useState(false);
-
   return(
-    <>
     <CardHoverStyled>
-      <li
-        onMouseEnter={() => setHoveredFav(true)}
-        onMouseLeave={() => setHoveredFav(false)}
-      >
-        <FavIco
-          color={hoveredFav ? 'red' : 'black'}
-        />
+      <li>
+        <BsHeartFill size={20} />
       </li>
-      <li
-        onMouseEnter={() => setHoveredSearch(true)}
-        onMouseLeave={() => setHoveredSearch(false)}
-      >
-        
+      <li>
         <Link to={`/detail/${id}`}>
-          <SearchIco
-            color={hoveredSearch ? 'red' : 'black'}  
-          />
+          <BsSearch size={20} />
         </Link>
         </li>
     </CardHoverStyled>
-    </>
   );
 }
 
