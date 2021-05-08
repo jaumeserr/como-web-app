cb78d8e2971e470e7bd169df2f816763b2b395b7
Jaume Serradell <jaumeserr@gmail.com>
2021-05-08 23:21:18 +0200

    minor changes

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,4 +1,4 @@
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 import { useState, useEffect } from "react";
 import { ToastContainer } from 'react-toastify';
 
